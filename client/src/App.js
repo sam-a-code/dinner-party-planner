08d8b6687dcec6acd5b33bed3c45f65e4edeeae2
Sam Avery <sam.avery1@gmail.com>
2022-11-22 09:57:47 -0600

    Configured client-side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect} from "react"
+import { BrowserRouter, Switch, Route, Routes } from "react-router-dom";
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,9 +14,19 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <h1>Page Count: {count} </h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/testing">
+            <h1>Test Route</h1>
+          </Route>
+          <Route path="/">
+            <h1>Page Count: {count}</h1>
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
